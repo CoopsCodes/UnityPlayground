c0dd8ae2b2a79f10f9fec505486c40b41049d67b
Cooper Viktor <im.cooperviktor@gmail.com>
2020-06-23 13:43:12 +1000

    character movement

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public CharacterController controller;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public float speed = 6f;
+
+	// Update is called once per frame
+	void Update()
+	{
+		float horizontal = Input.GetAxisRaw("Horizontal"); // Assigns the X input axis
+		float vertical = Input.GetAxisRaw("Vertical"); // Assigns the Z input axis
+		Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized; // Passes in the axis' and sets Y to 0 as it wont be used
+
+		if (direction.magnitude >= 0.1f)
+		{
+			controller.Move(direction * speed * Time.deltaTime);
+		}
+	}
 }
